5e89f36b70e3e09082033555eb352826a23a3ef6
Mohammed Arshad <arshadkhan2631@gmail.com>
2023-09-30 14:54:54 +0530

    Added phpunit test.

---
+++ web/modules/custom/gh_drupal_test/tests/src/Unit/MymoduleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\Tests\gh_drupal_test\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests the MyService class.
+ *
+ * @group mymodule
+ */
+class MymoduleTest extends UnitTestCase {
+
+  /**
+   * Tests the MyService::addNumbers() method.
+   */
+  public function testAddNumbers() {
+    $this->assertEquals(5, 5);
+  }
+}
+
