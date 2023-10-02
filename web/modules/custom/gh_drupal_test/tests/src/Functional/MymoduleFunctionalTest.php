831d8fb803b3d110b7f967e65fbe41042c47ca84
Mohammed Arshad <arshadkhan2631@gmail.com>
2023-10-02 21:24:50 +0530

    Added functional test suit.

---
+++ web/modules/custom/gh_drupal_test/tests/src/Functional/MymoduleFunctionalTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\Tests\gh_drupal_test\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Evaluates the functionality of the MyModule module.
+ *
+ * @group mymodule
+ */
+class MymoduleFunctionalTest extends BrowserTestBase {
+
+  /**
+   * Modules to be installed for this test.
+   *
+   * @var array
+   */
+  protected static $modules = ['mymodule'];
+
+  /**
+   * A user account with the required permissions for testing.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $webUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Generate a user with the necessary permissions.
+    $this->webUser = $this->drupalCreateUser([
+      'administer mymodule settings',
+    ]);
+  }
+
+  /**
+   * Assess the functionality of mymodule.
+   */
+  public function testMymoduleFunctionality() {
+    // Log in as the webUser.
+    $this->drupalLogin($this->webUser);
+
+    // Navigate to a page provided by your module.
+    $this->drupalGet('path-provided-by-mymodule');
+
+    // Confirm that the page loads successfully.
+    $this->assertResponse(200);
+
+    // Implement additional assertions as needed.
+  }
+}
